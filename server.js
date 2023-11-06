b86937c492a4317f406211e7ac884dba158b5437
AlexanderKamynin <kamyninsasa@gmail.com>
2023-11-06 21:53:16 +0300

    simple use socket.io

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
+const socket = require("socket.io");
+
 
 const PORT = 3000;
 https_options = {
@@ -29,7 +31,22 @@ const {createServer} = require("https");
 app.use(require("express").static(__dirname));
 app.use("/", routes);
 
-
 const SERVER = createServer(https_options, app);
+
+const io = socket(SERVER, {
+    cors: {
+        origin: "*",
+        methods: "*"
+    }
+})
+io.on("connection", socket => {
+    socket.on('message', message => {
+        console.log('получил данные');
+
+        io.sockets.emit('server_msg', `получил твою смсчку: ${message}`);
+    })
+})
+
+
 SERVER.listen(PORT);
 console.log(`Server working using ${PORT} port on https://localhost:${PORT}/`);
